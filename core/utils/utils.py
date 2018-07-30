6a0c7f0e294aaff768835b037a58a8027468fa30
Polianok Bogdan <miner34006@gmail.com>
2018-07-30 23:22:22 +0300

    add utils

---
+++ core/utils/utils.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+"""
+Created on 30.07.2018
+
+:author: Polianok Bogdan
+"""
+
+from core.skyskannerClasses.Leg import Leg
+
+
+def findLegs(jsonData, legId):
+    """
+    find leg in json with ID
+
+    :param jsonData: data where find leg
+    :param legId: id of leg to find
+    :return: leg object with legId
+    :rtype" skyskannerClasses.Leg.Leg
+    """
+    return [Leg(leg) for leg in jsonData['legs'] if leg['id'] == legId]
+
