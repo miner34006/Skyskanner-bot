ed01a9fd8e818196ae20ebac1d2c9771620bc3a1
Polianok Bogdan <miner34006@gmail.com>
2018-08-25 21:34:34 +0300

    add constants

--- vkApi/constants.py
+++ vkApi/constants.py
@@ -1,15 +1,58 @@
 # coding: utf-8
 
 """
-Created on **.**.2018
+Created on 25.08.2018
 
 :author: Polianok Bogdan
 """
 
-
-class EventStructure:
-    eventCode = 0
+ADD_MESSAGE = 4
 
 
-class Events:
-    ADD_MESSAGE = 4
+class ButtonsEnum:
+    """
+    Enum with different button types
+    """
+    back = {
+        'action': {
+            'type': 'text',
+            'label': 'Назад',
+        },
+        'color': 'negative'
+    }
+
+    source = {
+        'action': {
+            'type': 'text',
+            'label': 'Откуда',
+        },
+        'color': 'primary'
+    }
+
+    target = {
+        'action': {
+            'type': 'text',
+            'label': 'Куда',
+        },
+        'color': 'primary'
+    }
+
+    search = {
+        'action': {
+            'type': 'text',
+            'label': 'Показать самые дешевый билет',
+        },
+        'color': 'positive'
+    }
+
+
+cities = {
+    'Владивосток': 'VVO',
+    'Москва': 'MOSC',
+    'Санкт-Петербург': 'LED',
+    'Сочи': 'AAQ',
+    'Екатеринбург': 'SVX',
+    'Париж': 'PARI',
+    'Ярославль': 'IAR',
+}
+
