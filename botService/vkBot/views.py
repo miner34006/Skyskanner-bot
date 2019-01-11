89a1deec3ded2994e630be2a2c1d8535a62b18aa
Polianok Bogdan <miner34006@gmail.com>
2019-01-12 00:15:39 +0300

    add api endpoints for bot service

--- botService/vkBot/views.py
+++ botService/vkBot/views.py
@@ -1,19 +1,57 @@
-from flask import request
-from flask import Response
+import json
+
+import requests
+from flask import request, session, Response
 
 from botService.vkBot import app
+from botService.vkBot.UserSession import UserSession, SessionEncoder, asSession
 from vkApi.api import apiRequest
 
 
 @app.route('/send', methods=['POST'])
 def sendMessage():
+    """ api endpoint to send message from bot to user
+
+    :return: api response with status code
+    :rtype: Response
+    """
     data = request.get_json()
-    if 'userId' not in data or 'message' not in data:
+    try:
+        payload = {
+            'user_id': data['userId'],
+            'message': data['message'],
+        }
+        apiRequest('messages.send', payload)
+        return Response(status=200)
+    except requests.RequestException as _:
+        return Response(status=500)
+    except KeyError as _:
         return Response(status=400)
 
-    payload = {
-        'user_id': data['userId'],
-        'message': data['message'],
-    }
-    apiRequest('messages.send', payload)
-    return Response(status=200)
+
+@app.route('/receive', methods=['POST'])
+def receiveMessage():
+    """ api endpoint to handle new message from user
+
+    :return: api response with status code
+    :rtype: Response
+    """
+    data = request.get_json()
+    userSession = UserSession() if str(data['userId']) not in session \
+        else json.loads(session[str(data['userId'])], object_hook=asSession)
+
+    userSession.execute(data['message'])
+    session[str(data['userId'])] = str(json.dumps(userSession, cls=SessionEncoder))
+
+    try:
+        payload = {
+            'user_id': data['userId'],
+            'message': userSession.getInstruction(),
+            'keyboard': json.dumps(userSession.getKeyboard(), ensure_ascii=False).encode("utf-8")
+        }
+        apiRequest('messages.send', payload)
+        return Response(status=200)
+    except requests.RequestException as _:
+        return Response(status=500)
+    except KeyError as _:
+        return Response(status=400)
