3056c6912f48b88bb62e3f90fd5fc65adbdc5737
Polianok Bogdan <miner34006@gmail.com>
2019-01-12 00:08:12 +0300

    add encoder and decoder for userSession class

--- botService/vkBot/UserSession.py
+++ botService/vkBot/UserSession.py
@@ -6,24 +6,18 @@ Created on 22.09.2018
 :author: Polianok Bogdan
 """
 
-from vkApi.Menu import MainMenu
+from json import JSONEncoder
 
+import botService.vkBot.Menu as Menu
 
 class UserSession:
     """
     class represented one session with single unique user (one session for each user)
     """
-    def __init__(self, userId, scanner):
-
-        # TODO get/set data from/to DB
+    def __init__(self):
         self._sourceCity = None
         self._targetCity = None
-
-        self.userId = userId
-        self.thread = None
-
-        self.menu = MainMenu(self)
-        self.scanner = scanner
+        self.menu = Menu.MainMenu()
 
     @property
     def sourceCity(self):
@@ -75,7 +69,7 @@ class UserSession:
         :return: instruction for user
         :rtype: str
         """
-        return self.menu.getInstruction()
+        return self.menu.getInstruction(self)
 
     def changeMenu(self, menu):
         """
@@ -99,5 +93,41 @@ class UserSession:
         :param action: action to execute
         :return: None
         """
-        self.menu.execute(action)
+        self.menu.execute(action, self)
 
+
+class SessionEncoder(JSONEncoder):
+    """
+    Encoder for session object
+    """
+    def default(self, o):
+        """ Function to encode UserSession object
+
+        :param o: session object
+        :type o: UserSession
+        :return: session dict
+        :rtype: dict
+        """
+        obj = o.__dict__
+        obj.update({'class': type(o).__name__})
+        return obj
+
+def asSession(dict):
+    """ Function to create UserSession object from dict
+
+    :param dict: dict represented UserSession obj
+    :type dict: dict
+    :return: user session object
+    :rtype: UserSession
+    """
+    if 'class' in dict and dict['class'] == 'UserSession':
+        userSession = UserSession()
+        userSession.sourceCity = dict['_sourceCity']
+        userSession.targetCity = dict['_targetCity']
+
+        menuClass = getattr(Menu, dict['menu']['class'])
+        menu = menuClass()
+        userSession.menu = menu
+        return userSession
+    else:
+        return dict
